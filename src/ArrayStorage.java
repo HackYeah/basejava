000f4c708ff51f3cd6ba76c79d78d0d0db962318
Jacob Yermalitski <yakauy@Jacobs-MacBook-Pro.local>
2019-02-11 00:01:54 -0800

    Solution for homework 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,65 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);;
     }
 
     void save(Resume r) {
+        for(int i = 0; i < storage.length; i++)
+            if (storage [i] == null) {
+                storage[i] = r;
+                break;
+            }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i <= storage.length-1; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i <= storage.length-1; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int newSize = 0;
+        int newIndex = 0;
+
+        for (int i = 0; i < storage.length-1; i++)    {
+            if (storage[i] != null) {
+                newSize++;
+            }
+        }
+
+        Resume [] fullResumes = new Resume[newSize];
+        for (int i = 0; i < storage.length-1; i++) {
+            if (storage[i] != null) {
+                fullResumes[newIndex] = storage[i];
+                newIndex++;
+            }
+        }
+        return fullResumes;
     }
 
     int size() {
-        return 0;
+        int newSize = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                newSize++;
+            }
+        }
+        return newSize;
     }
 }
+
