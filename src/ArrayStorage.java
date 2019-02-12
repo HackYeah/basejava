ed9b2b7b9ea65fec739b5ab871c9b16b7a6f3b03
Jacob Yermalitski <yakauy@Jacobs-MacBook-Pro.local>
2019-02-12 06:15:01 -0800

    Adjustments related to code review feedback

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,21 +5,24 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null);;
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
         for(int i = 0; i < storage.length; i++)
             if (storage [i] == null) {
                 storage[i] = r;
+                size++;
                 break;
             }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i <= storage.length-1; i++) {
+        for (int i = 0; i <= size; i++) {
             if (storage[i] != null && storage[i].toString().equals(uuid)) {
                 return storage[i];
             }
@@ -28,44 +31,26 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for (int i = 0; i <= storage.length-1; i++) {
+         for (int i = 0; i < size; i++) {
             if (storage[i] != null && storage[i].toString().equals(uuid)) {
-                storage[i] = null;
+                storage[i] = storage[size-1];
+                storage[size-1] = null;
+                size--;
             }
         }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int newSize = 0;
-        int newIndex = 0;
+        return Arrays.copyOfRange(storage, 0, size );
 
-        for (int i = 0; i < storage.length-1; i++)    {
-            if (storage[i] != null) {
-                newSize++;
-            }
-        }
-
-        Resume [] fullResumes = new Resume[newSize];
-        for (int i = 0; i < storage.length-1; i++) {
-            if (storage[i] != null) {
-                fullResumes[newIndex] = storage[i];
-                newIndex++;
-            }
-        }
-        return fullResumes;
     }
 
     int size() {
-        int newSize = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                newSize++;
-            }
-        }
-        return newSize;
+        return size;
     }
 }
 
