38883347d1716cd4a7a5ef372bf12db7b28feb94
Jacob Yermalitski <yakauy@Jacobs-MacBook-Pro.local>
2019-02-12 07:00:50 -0800

    Improvements based on 2nd code review

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -13,17 +13,13 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        for(int i = 0; i < storage.length; i++)
-            if (storage [i] == null) {
-                storage[i] = r;
-                size++;
-                break;
-            }
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
         for (int i = 0; i <= size; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+            if (storage[i].toString().equals(uuid)) {
                 return storage[i];
             }
         }
@@ -32,7 +28,7 @@ public class ArrayStorage {
 
     void delete(String uuid) {
          for (int i = 0; i < size; i++) {
-            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+            if (storage[i].toString().equals(uuid)) {
                 storage[i] = storage[size-1];
                 storage[size-1] = null;
                 size--;
@@ -45,7 +41,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOfRange(storage, 0, size );
+        return Arrays.copyOf(storage, size);
 
     }
 
