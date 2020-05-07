0c1358261e5a589cb52929cefc5dfead93dfc3b3
Hackyeah <yermalitski@gmail.com>
2020-05-07 10:12:52 +0200

    Introduce interface

---
+++ src/ru/javaops/webapp/storage/Storage.java
@@ -0,0 +1,25 @@
+package ru.javaops.webapp.storage;
+
+import ru.javaops.webapp.model.Resume;
+
+/**
+ * Array based storage for Resumes
+ */
+public interface Storage {
+
+    void clear();
+
+    void save(Resume resume);
+
+    void update(Resume resume);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+
+}
+
